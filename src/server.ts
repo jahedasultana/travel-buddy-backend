dba47419254d513459d37e88f179b95114a29f42
Jaheda Sultana <jahedasultana47@gmail.com>
2025-12-12 10:38:06 +0600

    type deploy fixed

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import { envVars } from "./app/config/env.config"
 const startServer = async () => {
     try {
         app.listen(envVars.PORT || 5000, () => {
-            console.log(`Server is listening to port ${envVars.PORT}`)
+            console.log(`travel buddy server running`)
         })
     } catch (error) {
         console.log('Error while database connection.', error)
